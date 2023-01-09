4600fe944655bda3aa525c8d2eda52bda176ced8
Kled <83483378+1017yu@users.noreply.github.com>
2023-01-09 17:02:15 +0900

    #013

--- #011~#020/문자열안에 문자열.js
+++ #011~#020/문자열안에 문자열.js
@@ -1,7 +1,7 @@
 /*
 문자열 str1, str2가 매개변수로 주어집니다. str1 안에 str2가 있다면 1을 없다면 2를 return하도록 solution 함수를 완성해주세요.
 
- => Array.prototype.includes() 메소드 사용
+ => Array.prototype.includes() 메소드 사용 인지
 */
 
 function solution(str1, str2) {
