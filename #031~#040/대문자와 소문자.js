f72b3ce33fcadd16e95acc2393acfbaeab1fa9fd
Kled <83483378+1017yu@users.noreply.github.com>
2023-02-16 16:45:55 +0900

    #040

--- #031~#040/대문자와 소문자.js
+++ #031~#040/대문자와 소문자.js
@@ -17,7 +17,7 @@ function solution(my_string) {
 }
 
 
-/* 참고할 만한 다른 풀이 - map을 이용한 변환
+/* 참고할 만한 다른 풀이 - map을 이용한 string 변환
 function solution(my_string) {
     return my_string.split('').map(n => n === n.toUpperCase() ? n.toLowerCase() : n.toUpperCase()).join('')
 }
