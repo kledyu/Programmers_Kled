806347ba0ff91e57b27a71030362de2314e42817
Kled <83483378+1017yu@users.noreply.github.com>
2023-02-16 16:44:36 +0900

    #036

--- #031~#040/영어가 싫어요.js
+++ #031~#040/영어가 싫어요.js
@@ -6,15 +6,32 @@
 function solution(numbers) {
     
     let str = numbers.replaceAll('one', '1')
-    .replace('two', '2')
-    .replace('three', '3')
-    .replace('four', '4')
-    .replace('five', '5')
-    .replace('six', '6')
-    .replace('seven', '7')
-    .replace('eight', '8')
-    .replace('nine', '9')
-    .replace('zero', '0')
+    .replaceAll('two', '2')
+    .replaceAll('three', '3')
+    .replaceAll('four', '4')
+    .replaceAll('five', '5')
+    .replaceAll('six', '6')
+    .replaceAll('seven', '7')
+    .replaceAll('eight', '8')
+    .replaceAll('nine', '9')
+    .replaceAll('zero', '0')
                 
     return parseInt(str)
 }
+
+/* 참고해야할 다른 풀이 - 객체의 활용, replace 내 콜백함수의 활용
+
+function solution(numbers) {
+    const obj = {
+        zero: 0, one: 1, two: 2, three: 3, four: 4,
+        five: 5, six: 6, seven: 7, eight: 8, nine: 9
+    };
+
+    const num = numbers.replace(/zero|one|two|three|four|five|six|seven|eight|nine/g, (v) => {
+        return obj[v];
+    });
+
+    return Number(num);
+}
+
+*/
