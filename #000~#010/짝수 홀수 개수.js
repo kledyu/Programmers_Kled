ec6ef4fb85699d50218b1a83f2d671507729c8ac
Kled <83483378+1017yu@users.noreply.github.com>
2023-03-06 13:41:04 +0900

    #010

--- #000~#010/짝수 홀수 개수.js
+++ #000~#010/짝수 홀수 개수.js
@@ -1,5 +1,6 @@
 /*
-정수가 담긴 리스트 num_list가 주어질 때, num_list의 원소 중 짝수와 홀수의 개수를 담은 배열을 return 하도록 solution 함수를 완성해보세요.
+정수가 담긴 리스트 num_list가 주어질 때, 
+num_list의 원소 중 짝수와 홀수의 개수를 담은 배열을 return 하도록 solution 함수를 완성해보세요.
 */
 
 function solution(num_list) {
