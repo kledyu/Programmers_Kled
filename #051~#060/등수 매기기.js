64da756a837e9b0d058ab6ee4ecf62d80283a159
Kled <83483378+1017yu@users.noreply.github.com>
2023-03-10 15:06:28 +0900

    #055

--- #051~#060/등수 매기기.js
+++ #051~#060/등수 매기기.js
@@ -24,7 +24,7 @@ function solution(score) {
     let avg = score.map(v => (v[0] + v[1]) / 2);
     let sorted = avg.slice().sort((a, b) => b - a);
 
-    return avg.map(v => sorted.indexOf(v) + 1) 
+    return avg.map(v => sorted.indexOf(v) + 1)
 }
 
 */
