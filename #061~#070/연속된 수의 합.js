e1fd0fcbfcfdf25f6694638ab574ec1133738e99
Kled <83483378+1017yu@users.noreply.github.com>
2023-03-18 11:21:22 +0900

    #061

---
+++ #061~#070/연속된 수의 합.js
@@ -0,0 +1,10 @@
+/*
+연속된 세 개의 정수를 더해 12가 되는 경우는 3, 4, 5입니다. 
+두 정수 num과 total이 주어집니다. 연속된 수 num개를 더한 값이 total이 될 때, 
+정수 배열을 오름차순으로 담아 return하도록 solution함수를 완성해보세요.
+*/
+
+const solution = (num, total) => { 
+    return new Array(num)
+        .fill(Math.ceil(total/num) - Math.floor(num / 2))
+        .map((v, i) => v + i)}
