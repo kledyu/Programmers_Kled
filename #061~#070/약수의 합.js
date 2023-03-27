bb2a9b62c84e62cb691654966474c2ec81f77db7
Kled Yu <83483378+1017yu@users.noreply.github.com>
2023-03-27 15:48:23 +0900

    #065

---
+++ #061~#070/약수의 합.js
@@ -0,0 +1,13 @@
+/*
+정수 n을 입력받아 n의 약수를 모두 더한 값을 리턴하는 함수, solution을 완성해주세요.
+*/
+
+const solution = (n) => {
+    let answer = 0;
+    for(let i = 1; i <= n; i++) {
+        if (n % i === 0) {
+            answer += i
+        }
+    }
+    return answer;
+}
