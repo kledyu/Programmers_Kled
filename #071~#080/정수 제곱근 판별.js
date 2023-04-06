62670254e77b4f083f3126eecb7d39bdb08616b1
Kled Yu <83483378+1017yu@users.noreply.github.com>
2023-04-06 15:23:44 +0900

    #072

---
+++ #071~#080/정수 제곱근 판별.js
@@ -0,0 +1,9 @@
+/*
+임의의 양의 정수 n에 대해, n이 어떤 양의 정수 x의 제곱인지 아닌지 판단하려 합니다.
+n이 양의 정수 x의 제곱이라면 x+1의 제곱을 리턴하고,
+n이 양의 정수 x의 제곱이 아니라면 -1을 리턴하는 함수를 완성하세요.
+*/
+
+const solution = (n) => {
+    return [...Math.sqrt(n).toString()].includes('.') ? -1 : Math.pow((Math.sqrt(n) + 1), 2)
+}
