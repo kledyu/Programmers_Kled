89593f0883a4bbb34a904040c95efacde5530a19
Kled Yu <83483378+1017yu@users.noreply.github.com>
2023-04-08 10:55:57 +0900

    #075

---
+++ #071~#080/문자열을 정수로 바꾸기.js
@@ -0,0 +1,21 @@
+/*
+문자열 s를 숫자로 변환한 결과를 반환하는 함수, solution을 완성하세요.
+*/
+
+
+/* string과 산술 연산 시, 암시적 형변환으로 string을 number로 취급 */
+const solution = (s) => {
+    return s * 1
+}
+
+/* String to Number
+
+const solution = (s) => {
+    return Number(s)
+}
+
+const solution = (s) => {
+    return parseInt(s)
+}
+
+*/
