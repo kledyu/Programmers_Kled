ce5dd4be2cbd394146bf509021de2b13ec40bceb
Kled Yu <83483378+1017yu@users.noreply.github.com>
2023-05-01 10:32:39 +0900

    #093

---
+++ #091~#100/제일 작은 수 제거하기.js
@@ -0,0 +1,18 @@
+/*
+정수를 저장한 배열, arr 에서 가장 작은 수를 제거한 배열을 리턴하는 함수, solution을 완성해주세요.
+단, 리턴하려는 배열이 빈 배열인 경우엔 배열에 -1을 채워 리턴하세요. 예를들어 arr이 [4,3,2,1]인 경우는 [4,3,2]를 리턴 하고, [10]면 [-1]을 리턴 합니다.
+*/
+
+const solution = (arr) => {
+    const newArr = [...arr]
+    arr.sort((a, b) => a - b)
+    return arr.length !== 1 ? newArr.filter(v => v > arr[0]) : [-1]
+
+}
+/* 참고할 만한 다른 풀이
+const solution = (arr) => {
+    arr.splice(arr.indexOf(Math.min(...arr)), 1);
+    if(arr.length < 1) return[-1] ;
+    return arr;
+}
+*/
