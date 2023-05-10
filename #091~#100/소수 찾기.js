a86dec38ac5aff83888c9d648e993ca625cbd1f8
Kled Yu <83483378+1017yu@users.noreply.github.com>
2023-05-10 23:58:28 +0900

    #097

---
+++ #091~#100/소수 찾기.js
@@ -0,0 +1,32 @@
+/*
+1부터 입력받은 숫자 n 사이에 있는 소수의 개수를 반환하는 함수, solution을 만들어 보세요.
+
+소수는 1과 자기 자신으로만 나누어지는 수를 의미합니다.
+(1은 소수가 아닙니다.)
+*/
+
+function solution(n) {
+    let numbers = []
+    let answer = []
+    const root = Math.sqrt(n)
+
+    for (let a = 0; a <= n; a++) {
+        numbers.push(true)
+    }
+    numbers[0] = false
+    numbers[1] = false
+
+    // 합성수는 false로 교체
+    for (let i = 2; i < root; i++) {
+        if (numbers[i]) {            
+            for (let j = i * i; j <= n; j += i) {
+                numbers[j] = false;
+                
+                console.log(j)
+            }
+        }
+    }
+
+    answer = numbers.filter(num => num === true)
+    return answer.length
+}
